94ce5a3e6a93dc846caf026c640b70114ef2baeb
rchl-m <rachel_montelibano@yahoo.com>
2026-09-25 01:12:38 +0800

    Add files via upload

---
+++ assets/Sa92nDG-8aEOxCjO/story_content/triggers.js
@@ -0,0 +1,76 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6gNmnc9Bruy":
+        Script1();
+        break;
+      case "5x1plonhmeA":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('6O5Yr3a61ah');
+const duration = 5000;
+const easing = 'ease-out';
+const id = '5ocQtVkDIkf';
+const pulseAmount = 0.03;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5ifVuBbw7Wa');
+const duration = 500;
+const easing = 'ease-out';
+const id = '6koO4Y20WpX';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
